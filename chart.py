c4330f97f8fe60073f454f814d2a9848c361791d
Henry Thasler <git@thasler.org>
2017-02-04 18:50:23 +0100

    initial commit

---
+++ chart.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# called by cron-job
+
+import os
+import time
+
+import plotly.offline as py
+import plotly.graph_objs as go
+
+import numpy as np
+
+from datetime import datetime, timedelta
+from subprocess import call
+
+import paho.mqtt.client as mqtt
+import struct
+
+def to_unix_time(dt):
+    epoch =  datetime.utcfromtimestamp(0)
+    return (dt - epoch).total_seconds() * 1000
+
+ROOTDIR = "/home/henry/pyLogger"
+
+
+N = 10
+
+x = []
+y1 = []
+y2 = []
+y3 = []
+
+y1_avg = []
+y2_avg = []
+
+
+with open(ROOTDIR+'/data.csv', "rb") as csvfile:
+  last_temperature = 0;
+  last_humidity = 0;
+  timeframe = datetime.now() - timedelta(days=7)
+  timeframe = timeframe.replace(second=0, microsecond=0,minute=0,hour=0)
+  
+  csvfile.seek(-60*24*8*34, os.SEEK_END)
+  next(csvfile)
+  for line in csvfile:
+    row = line.rstrip().split(";")
+    try:
+      csv_temperature = float(row[1])
+      csv_humidity = float(row[2])
+      if row[3] == '-':
+        csv_ext_temperature = None
+      else:
+        csv_ext_temperature = float(row[3])
+      csv_time = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+      
+      if csv_time >= timeframe:
+        x.append(csv_time)
+        y3.append(csv_ext_temperature)   
+                
+        if len(y1)>0:
+          if (abs(csv_temperature - last_temperature) < 1.0):            
+            y1.append(csv_temperature)
+            last_temperature = csv_temperature
+          else:
+#            print('skipped', csv_temperature)
+            y1.append(last_temperature)
+#            last_temperature = csv_temperature
+        else:
+          y1.append(csv_temperature)
+          last_temperature = csv_temperature
+
+
+        if len(y2)>0:
+          if (abs(csv_humidity - last_humidity) < 5):
+            y2.append(csv_humidity)
+            last_humidity = csv_humidity
+          else:
+            y2.append(last_humidity)
+        else:
+          y2.append(csv_humidity)
+          last_humidity = csv_humidity
+          
+    except:
+      pass
+    
+traces = []
+
+traces.append(go.Scatter(
+        x=x,
+#        y=y1,
+        y=np.convolve(y1, np.ones((N,))/N, mode='valid'),
+#        mode = 'lines+markers',
+        mode = 'lines',
+        marker = dict(
+          maxdisplayed= 100
+          ),
+        line = dict(
+          color = ('rgb(250, 20, 0)'),
+          width = 3,
+        ),
+        name = 'Innen',
+    ))
+
+traces.append(go.Scatter(
+        x=x,
+	y=y3,
+#        y=np.convolve(y3, np.ones((N,))/N, mode='valid'),
+#        mode = 'lines+markers',
+        mode = 'lines',
+        marker = dict(
+          maxdisplayed= 100
+          ),
+        line = dict(
+          color = ('rgb(0, 90, 200)'),
+          width = 3,
+        ),
+        name = 'Außen',
+    ))
+
+'''        
+traces.append(go.Scatter(
+        x=x,
+#        y=y2,
+        y=np.convolve(y2, np.ones((N,))/N, mode='valid'),
+        yaxis="y2",
+#        mode = 'lines+markers',
+        mode = 'lines',
+        marker = dict(
+          maxdisplayed= 100
+          ),
+        line = dict(
+          color = ('rgb(16, 196, 0)'),
+          width = 3,
+        ),
+        name = 'Luftfeuchtigkeit',
+    ))
+'''
+    
+layout = go.Layout(
+    autosize=False,
+    width=800,
+    height=600,
+    margin=dict(
+        l=50,
+        r=30,
+        b=80,
+        t=50,
+        pad=0
+    ),
+  
+    # https://plot.ly/javascript-graphing-library/reference/#xaxis
+    xaxis=dict(
+        showline=True,
+#        showgrid=False,
+        showticklabels=True,
+        linecolor='rgb(204, 204, 204)',
+        linewidth=2,
+        ticks='outside',
+        autotick=False,
+#        nticks=4,
+        range=[to_unix_time(timeframe), to_unix_time(timeframe + timedelta(days=8))],
+        tick0 = to_unix_time(timeframe.replace(second=0, microsecond=0,minute=0,hour=4)),
+        dtick=3600000*12,
+        tickcolor='rgb(204, 204, 204)',
+        tickwidth=2,
+        ticklen=5,
+        # https://github.com/d3/d3/wiki/Time-Formatting
+        tickformat='%a, %Hh',
+        tickfont=dict(
+#            family='Arial',
+            size=12,
+            color='rgb(82, 82, 82)',
+        ),
+    ),  
+    yaxis=dict(
+        linecolor='rgb(204, 204, 204)',
+        linewidth=2,
+        title=u'Temperatur [°C]',
+        showticklabels=True,
+        ticks='outside',
+        tickcolor='rgb(204, 204, 204)',
+        tickwidth=2,
+        ticklen=5,
+        tickfont=dict(
+#            family='Arial',
+            size=12,
+            color='rgb(82, 82, 82)',
+        ),
+    ),
+    showlegend=False,
+)
+'''        
+    yaxis2=dict(
+        linecolor='rgb(204, 204, 204)',
+        linewidth=2,
+        showgrid=False,
+        title='Luftfeuchtigkeit [%]',
+        overlaying='y',
+        side='right',
+        showticklabels=True,
+        ticks='outside',
+        tickcolor='rgb(204, 204, 204)',
+        tickwidth=2,
+        ticklen=5,
+        tickfont=dict(
+#            family='Arial',
+            size=12,
+            color='rgb(82, 82, 82)',
+        ),
+    )
+)
+'''
+
+annotations = []
+
+# Title
+annotations.append(dict(xref='paper', yref='paper', x=0.5, y=1.025,
+                              xanchor='center', yanchor='bottom',
+                              text='Innen- und Außentemperatur',
+                              font=dict(
+#                                        family='Arial',
+                                        size=30,
+                                        color='rgb(37,37,37)'),
+                              showarrow=False,))
+# Source
+annotations.append(dict(xref='paper', yref='paper', x=0.5, y=-0.12,
+                              xanchor='center', yanchor='top',
+                              text='Chart generated on ' + datetime.now().strftime("%Y-%m-%d %H:%M"),
+                              font=dict(
+#                                        family='Arial',
+                                        size=12,
+                                        color='rgb(150,150,150)'),
+                              showarrow=False,))
+
+layout['annotations'] = annotations
+    
+fig = go.Figure(data=traces, layout=layout)
+py.plot(fig, filename=ROOTDIR+'/temp-chart.html', auto_open=False, show_link=False)
+call(["/home/henry/jsPhotoFrame/phantomjs-armv7/bin/phantomjs",ROOTDIR+"/capture.js", "file:///"+ROOTDIR+"/temp-chart.html"]) 
+
+
+def on_connect(client, userdata, flags, rc):
+    #print("Connected to mqtt broker with result code "+str(rc))
+    with open(ROOTDIR+'/out.png') as f:
+      img = f.read()
+      f.close()
+      byteArray = bytearray(img)
+      client.publish("home/img/tempchart", byteArray, retain=True)
+      
+def on_publish(client, userdata, mid):
+  # Disconnect after our message has been sent.
+  client.disconnect()
+
+client = mqtt.Client('chart-%s' % os.getpid())
+client.on_connect = on_connect
+client.on_publish = on_publish
+
+client.connect("127.0.0.1")
+client.loop_start()
+time.sleep(2)
+client.loop_stop()
---
+++ log.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import time
+from datetime import datetime
+from struct import *
+import Adafruit_DHT
+import csv
+import paho.mqtt.client as mqtt
+import json
+
+millis = lambda: int(round(time.time() * 1000))
+octlit = lambda n:int(n, 8)
+start_time = time.time()
+runtime = lambda: '{0:0.2f}s'.format(time.time()-start_time) 
+
+log_lastupdate = millis()-60000
+ext_temperature = None
+int_humidity={
+  'value': None,
+  'timestamp': None,
+  'unit': '%'
+  }
+int_temperature={
+  'value': None,
+  'timestamp': None,
+  'unit': '°C'
+  }
+
+# set up csv logger
+logwriter = None
+csvfile = open('/home/henry/pyLogger/data.csv', 'ab')
+if csvfile:
+  logwriter = csv.writer(csvfile, delimiter=';')
+
+#setup mqtt stuff
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected to mqtt broker with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("home/out/temp")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global ext_temperature
+    
+#    print(msg.topic+" "+msg.payload)
+    ext_temperature = json.loads(msg.payload)
+    ext_temperature["timestamp"] = int(ext_temperature["timestamp"])
+    #print(" "+str(ext_temperature))
+    
+
+client = mqtt.Client("pyLogger")
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("127.0.0.1")
+client.loop_start()
+
+try:
+  while 1:
+        # save received data
+    if (millis() - log_lastupdate) > 60000:
+      log_lastupdate = millis()
+      int_humidity["value"], int_temperature["value"] = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+      int_humidity["value"] = int(int_humidity["value"] - 20)
+      int_temperature["value"] = round(int_temperature["value"], 1)
+      int_temperature["timestamp"] = int(time.time())
+      int_humidity["timestamp"] = int(time.time())
+      
+      #print(int_humidity, int_temperature)
+
+      client.publish("home/in/temp", json.dumps(int_temperature), retain=True)
+      client.publish("home/in/temp/value", '{0:0.1f}'.format(int_temperature["value"]), retain=True)
+      client.publish("home/in/hum", json.dumps(int_humidity), retain=True)
+      client.publish("home/in/hum/value", int_humidity["value"], retain=True)
+      
+      if (ext_temperature == None) or (( (millis()/1000 - ext_temperature["timestamp"]) > 600) and (ext_temperature["timestamp"] > 0)) :
+        logwriter.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '{0:0.1f}'.format(int_temperature["value"]), '{0:0.0f}'.format(int_humidity["value"]), '-'])
+      else:
+        logwriter.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '{0:0.1f}'.format(int_temperature["value"]), '{0:0.0f}'.format(int_humidity["value"]), '{0:0.1f}'.format(ext_temperature["value"])])
+
+      csvfile.flush()
+    time.sleep(1)
+except KeyboardInterrupt:
+  pass
+finally:
+  pass
