475e1818e6d666a97daa986fcfd10c015cb305a3
henrythasler <git@thasler.org>
2017-12-27 16:21:47 +0100

    updated logger to sqlite

--- log.py
+++ log.py
@@ -9,6 +9,7 @@ import Adafruit_DHT
 import csv
 import paho.mqtt.client as mqtt
 import json
+import sqlite3 as lite
 
 millis = lambda: int(round(time.time() * 1000))
 octlit = lambda n:int(n, 8)
@@ -18,21 +19,28 @@ runtime = lambda: '{0:0.2f}s'.format(time.time()-start_time)
 log_lastupdate = millis()-60001
 ext_temperature = None
 int_humidity={
-  'value': None,
-  'timestamp': None,
-  'unit': '%'
-  }
+    'value': None,
+    'timestamp': None,
+    'unit': '%'
+    }
 int_temperature={
-  'value': None,
-  'timestamp': None,
-  'unit': '°C'
-  }
+    'value': None,
+    'timestamp': None,
+    'unit': '°C'
+    }
 
 # set up csv logger
-logwriter = None
-csvfile = open('/home/henry/pyLogger/data.csv', 'ab')
-if csvfile:
-  logwriter = csv.writer(csvfile, delimiter=';')
+#logwriter = None
+#csvfile = open('/home/henry/pyLogger/data.csv', 'ab')
+#if csvfile:
+#  logwriter = csv.writer(csvfile, delimiter=';')
+
+try:
+    con = lite.connect('temperature.db', detect_types=lite.PARSE_DECLTYPES)
+    cur = con.cursor()
+except lite.Error, e:
+    print "Error %s:" % e.args[0]
+    sys.exit(1)
 
 #setup mqtt stuff
 # The callback for when the client receives a CONNACK response from the server.
@@ -46,10 +54,15 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global ext_temperature
+    global con, cur
     
 #    print(msg.topic+" "+msg.payload)
     ext_temperature = json.loads(msg.payload)
     ext_temperature["timestamp"] = int(ext_temperature["timestamp"])
+
+    cur.execute("INSERT INTO outside(time, temp, hum) VALUES(?, ?, ?)", (datetime.now(), ext_temperature["value"], None))
+    con.commit()
+    
     #print(" "+str(ext_temperature))
     
 
@@ -61,29 +74,33 @@ client.connect("localhost")
 client.loop_start()
 
 try:
-  while 1:
-        # save received data
-    if (millis() - log_lastupdate) > 60000:
-      log_lastupdate = millis()
-      int_humidity["value"], int_temperature["value"] = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
-      int_humidity["value"] = int(int_humidity["value"] - 20)
-      int_temperature["value"] = round(int_temperature["value"], 1)
-      int_temperature["timestamp"] = int(time.time())
-      int_humidity["timestamp"] = int(time.time())
-      
-      client.publish("home/in/temp", json.dumps(int_temperature), retain=True)
-      client.publish("home/in/temp/value", '{0:0.1f}'.format(int_temperature["value"]), retain=True)
-      client.publish("home/in/hum", json.dumps(int_humidity), retain=True)
-      client.publish("home/in/hum/value", int_humidity["value"], retain=True)
-      
-      if (ext_temperature == None) or (( (millis()/1000 - ext_temperature["timestamp"]) > 600) and (ext_temperature["timestamp"] > 0)) :
-        logwriter.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '{0:0.1f}'.format(int_temperature["value"]), '{0:0.0f}'.format(int_humidity["value"]), '-'])
-      else:
-        logwriter.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '{0:0.1f}'.format(int_temperature["value"]), '{0:0.0f}'.format(int_humidity["value"]), '{0:0.1f}'.format(ext_temperature["value"])])
+    while 1:
+          # save received data
+        if (millis() - log_lastupdate) > 60000:
+            log_lastupdate = millis()
+            int_humidity["value"], int_temperature["value"] = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+            int_humidity["value"] = int(int_humidity["value"] - 20)
+            int_temperature["value"] = round(int_temperature["value"], 1)
+            int_temperature["timestamp"] = int(time.time())
+            int_humidity["timestamp"] = int(time.time())
+            
+            client.publish("home/in/temp", json.dumps(int_temperature), retain=True)
+            client.publish("home/in/temp/value", '{0:0.1f}'.format(int_temperature["value"]), retain=True)
+            client.publish("home/in/hum", json.dumps(int_humidity), retain=True)
+            client.publish("home/in/hum/value", int_humidity["value"], retain=True)
 
-      csvfile.flush()
-    time.sleep(1)
+            cur.execute("INSERT INTO livingroom(time, temp, hum) VALUES(?, ?, ?)", (datetime.now(), int_temperature["value"], int_humidity["value"]))
+            con.commit()
+                
+#            if (ext_temperature == None) or (( (millis()/1000 - ext_temperature["timestamp"]) > 600) and (ext_temperature["timestamp"] > 0)) :
+#              logwriter.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '{0:0.1f}'.format(int_temperature["value"]), '{0:0.0f}'.format(int_humidity["value"]), '-'])
+#            else:
+#              logwriter.writerow([datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '{0:0.1f}'.format(int_temperature["value"]), '{0:0.0f}'.format(int_humidity["value"]), '{0:0.1f}'.format(ext_temperature["value"])])
+
+#            csvfile.flush()
+        time.sleep(1)
 except KeyboardInterrupt:
-  pass
+    pass
 finally:
-  pass
+    if con:
+        con.close()            
